d42bc45a734cf89adae494c09b2c5744908c2da2
uumlaut <39193849+vyragosa@users.noreply.github.com>
2024-04-01 18:03:16 +0300

    Add properties to Keeboard input

--- Assets/Just-a-Heli/Code/Scripts/Input/KeyboardHeli_Input.cs
+++ Assets/Just-a-Heli/Code/Scripts/Input/KeyboardHeli_Input.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace JaH
 {
     public class Keyboard_Input : BaseHeli_Input
     {
         #region Variables
-        [Header("Heli Keyboard Inputs")]
-        public float throttleInput = 0f;
-        public float collectiveInput = 0f;
-        public float cyclicInput = 0f;
-        public float pedalInput = 0f;
+        // [Header("Heli Keyboard Inputs")]
         #endregion
+
+        #region Properties
+
+        private float throttleInput = 0f;
+        public float ThrottleInput
+        {
+            get { return throttleInput; }
+        }
+
+        private float collectiveInput = 0f;
+        public float CollectiveInput
+        {
+            get { return collectiveInput; }
+        }
+
+        private float cyclicInput = 0f;
+        public float CyclicInput
+        {
+            get { return cyclicInput; }
+        }
+
+        private float pedalInput = 0f;
+        public float PedalInput
+        {
+            get { return pedalInput; }
+        }
+        #region
 
         #region BuildIn Methods
         #endregion
