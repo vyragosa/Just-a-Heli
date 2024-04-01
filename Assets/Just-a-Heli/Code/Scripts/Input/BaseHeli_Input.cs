03290b3d38127c62dc5ee1198557606d66cf5337
uumlaut <39193849+vyragosa@users.noreply.github.com>
2024-04-01 16:10:18 +0300

    Stubbling input

---
+++ Assets/Just-a-Heli/Code/Scripts/Input/BaseHeli_Input.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JaH
+{
+    public class BaseHeli_Input : MonoBehaviour
+    {
+        #region Variables
+        [Header("Base Input Properties")]
+        public float vertical = 0f;
+        public float horizontal = 0f;
+        #endregion
+
+        #region BuildIn Methods
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleInputs();
+        }
+        #endregion
+
+        #region CustomMethods
+        protected virtual void HandleInputs()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+        }
+        #endregion
+    }
+
+}
