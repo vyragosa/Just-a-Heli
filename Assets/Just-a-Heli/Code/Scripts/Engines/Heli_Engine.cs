ea7e88cf83d9becf030a890dbf8baab4daa31a94
uumlaut <39193849+vyragosa@users.noreply.github.com>
2024-04-01 22:17:38 +0300

    Create helicopter engine

---
+++ Assets/Just-a-Heli/Code/Scripts/Engines/Heli_Engine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JaH
+{
+    public class Heli_Engine : MonoBehaviour
+    {
+        #region Variables
+        public float maxHP = 140f;
+        public float maxRPM = 2700;
+        public float powerDelay = 2f;
+        #endregion
+
+        #region Properties
+        private float currentHP;
+        public float CurrentHP
+        {
+            get { return currentHP; }
+        }
+
+        private float currentRPM;
+        public float CurrentRPM
+        {
+            get { return currentRPM; }
+        }
+
+        #endregion
+
+        #region BuildIn methods
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+
+        #endregion
+
+        #region Custom Methods
+        public void UpdateEngine(float throttleInput)
+        {
+            
+        }
+        #endregion
+    }
+
+}
