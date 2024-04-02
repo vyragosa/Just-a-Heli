b3f86c96155d2201db8d89fae9536d52ea63b67c
uumlaut <39193849+vyragosa@users.noreply.github.com>
2024-04-02 20:50:37 +0300

    Edit blur turn on and off

--- Assets/Just-a-Heli/Code/Scripts/Rotors/Rotor_Blur.cs
+++ Assets/Just-a-Heli/Code/Scripts/Rotors/Rotor_Blur.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace JaH
@@ -8,12 +10,13 @@ namespace JaH
     {
         #region Variables
         [Header("Roor Blur Properties")]
-        public float maxDPS = 100f;
+        public float maxDPS = 30f;
         public List<GameObject> blades = new List<GameObject>();
         public GameObject blurGeo;
 
 
         public List<Texture2D> blureTextures = new List<Texture2D>();
+        public Material blurMat;
         #endregion
         #region Interface Methods
         public void UpdateRotor(float dps, Input_Controller input)
@@ -24,8 +27,33 @@ namespace JaH
             // вычисление текстуры по кадрам
             int blurTexID = Mathf.FloorToInt(normalizedDPS * blureTextures.Count - 1);
             blurTexID = Mathf.Clamp(blurTexID, 0, blureTextures.Count - 1);
+
+            if(blurMat && blureTextures.Count > 0 )
+            {
+                blurMat.SetTexture("_MainTex", blureTextures[blurTexID]);
+            }
+
+            if (blurTexID > 2 && blades.Count > 0) 
+            {
+                HandleGeoBladeViz(false);
+            }
+            else
+            {
+                HandleGeoBladeViz(true);
+            }
         }
         #endregion
+
+        #region Custom Methods
+        void HandleGeoBladeViz(bool viz)
+        {
+            foreach (var blade in blades)
+            {
+                blade.SetActive(viz);
+            }
+        }
+        #endregion
+
 
     }
 
